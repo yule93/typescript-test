2c7497cf898c92b6ed032fd9707b9b14226fc7a1
yule93 <dbfm2222@gmail.com>
2021-05-03 23:34:50 +0900

    study about interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+interface IState {
+  counter: number;
+}
+
+class App extends Component<{}, IState> {
+  state = {
+    counter: 0
+  };
+  render() {
+    const { counter } = this.state;
+    return <div>{ counter } <button onClick = {this.add}>Add</button></div>
+  }
+
+  // void 타입 함수을 선언하는 typescript 문법
+  add = () :void => {
+    this.setState(prev => {
+      return {
+        counter: prev.counter + 1
+      };
+    });
+  };
 }
 
 export default App;
