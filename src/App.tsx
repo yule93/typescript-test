ce523dc16dce8ff80fd7cb34e9c935b20d021a92
yule93 <dbfm2222@gmail.com>
2021-05-19 23:16:50 +0900

    make functional interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import React, {Component} from 'react';
 import Number from "./Number";
+import {Input, Form} from "./Input";
 
 interface IState {
   counter: number;
+  name: string;
 }
 
 class App extends Component<{}, IState> {
   state = {
-    counter: 0
+    counter: 0,
+    name: ""
   };
   render() {
-    const { counter } = this.state;
+    const { counter, name } = this.state;
     return (
       <div>
+        <Form onFormSubmit = {this.onFormSubmit}>
+          <Input value = {name} onChange = {this.onChange}/>
+        </Form>
         <Number count = {counter}/> <button onClick = {this.add}>Add</button>
       </div>
     );
+  }
+
+  // event 매개변수는 argument
+  onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    console.log(event.target);
+  }
+
+  onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
   }
 
   // void 타입 함수을 선언하는 typescript 문법
