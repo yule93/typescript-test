8a3cb0e5a72cab8b57c03c2a78672d7a9d9124b9
yule93 <dbfm2222@gmail.com>
2021-05-20 23:35:38 +0900

    .

--- src/Number.tsx
+++ src/Number.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.span``;
+const Container = styled.span<{isBlue: boolean}`
+    color: ${props => props.isBlue ? "blue" : "black"};
+`;
 
 interface IProps {
     count: number;
 }
 
-const Number: React.FunctionComponent<IProps> = ({count}) => <Container>{count}</Container> // IProps 대신에 count: number을 넣어줘도 됨
+const Number: React.FunctionComponent<IProps> = ({count}) => <Container isBlue = {count > 10 ? }>{count}</Container> // IProps 대신에 {count: number}을 넣어줘도 됨
 
 export default Number;
